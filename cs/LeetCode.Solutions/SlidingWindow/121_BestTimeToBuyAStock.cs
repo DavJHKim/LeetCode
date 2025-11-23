0d1edd26ed9bb10ead32e5713ee220a280a22405
David Kim <djhkim2003@gmail.com>
2025-11-22 22:35:28 -0300

    added boilerplate to BestTimeToBuyAStock

---
+++ cs/LeetCode.Solutions/SlidingWindow/121_BestTimeToBuyAStock.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace LeetCode.Solutions;
+
+public class BestTimeToBuyAStockSolutions
+{
+    public 
+}
---
+++ cs/LeetCode.Tests/SlidingWindow/121_BestTimeToBuyAStockTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using LeetCode.Solutions;
+
+namespace LeetCode.Test;
+
+public class BestTimeToBuyAStockTests
+{
+    [Fact]
+    public void Test1()
+    {
+        int[] prices = new int[] {10,1,5,6,7,1}
+
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        int[] prices = new int[] {10,8,7,5,2}
+
+    }
+}
